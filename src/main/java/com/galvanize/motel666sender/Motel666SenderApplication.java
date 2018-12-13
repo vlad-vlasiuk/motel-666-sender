e155c0fe238afa874aa6eb3e2e1575313d5c5e69
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-13 16:11:18 -0700

    create spring boot app
    core: DevTools, Lombok
    web: Web
    integration: RabbitMQ

---
+++ src/main/java/com/galvanize/motel666sender/Motel666SenderApplication.java
@@ -0,0 +1,14 @@
+package com.galvanize.motel666sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Motel666SenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Motel666SenderApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/galvanize/motel666sender/Motel666SenderApplicationTests.java
@@ -0,0 +1,17 @@
+package com.galvanize.motel666sender;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Motel666SenderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
